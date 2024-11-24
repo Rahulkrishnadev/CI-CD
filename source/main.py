acbb44bf7c21378161f0c25c65c7b552257e905f
Rahul <reboundrahul777@gmail.com>
2024-11-24 20:47:51 +0530

    subtract

--- source/main.py
+++ source/main.py
@@ -2,3 +2,7 @@ def add_numbers(a,b):
     return a+b
 
 
+def sub_numbers(a,b):
+    return a-b    
+
+
--- tescase/test_main.py
+++ tescase/test_main.py
@@ -1,6 +1,11 @@
-from source.main import add_numbers
+from source.main import add_numbers,sub_numbers
 
 def test_answer():
     assert add_numbers(3,4) == 7
     assert add_numbers(4,5) == 9
     assert add_numbers(5,5) == 10
+
+def test_ans():
+    assert sub_numbers(3,4) == -1
+    assert sub_numbers(4,5) == -1
+    assert sub_numbers(5,5) == 0    
